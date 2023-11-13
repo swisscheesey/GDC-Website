8787b3795ea7efda8fdecc6d4ce87fde3d425679
tgeo99 <tgeo1zda2@gmail.com>
2023-11-13 13:28:58 -0600

    Calendar and Comments
    Calendar now has same header as other pages. Styling is a little off, but will be fixed later
    Comment message content can be styled individually
    Comments can be expanded and hidden
    Button styling on comments
    Capitalization changes
    Reply box is now directly underneath comment being replied to

--- comments.js
+++ comments.js
@@ -8,7 +8,7 @@ loadComments();
 var commentTarget = document.getElementById("comments");
 function loadComments(){
     document.getElementById("commentsArea").innerHTML = commentsHTML;
-    document.getElementById("addComment").innerHTML = addCommentHTML;
+    document.getElementById("addComment").innerHTML = "<b>Add a comment...</b><br/>" + addCommentHTML;
     previousComments = localStorage.getItem(window.location.href);
     if(previousComments === null) return;
     document.getElementById("comments").innerHTML = previousComments;
@@ -22,24 +22,32 @@ function addComment(ev){
     nameField = document.getElementById('name').value;
     if(commentText === '') return;
     if(nameField === '') return;
-    const posterName = document.createElement('h3');
-    posterName.className = 'name';
-    const textBox = document.createElement('p');
-    const replyButton = document.createElement('button');
-    replyButton.className = 'reply';
-    replyButton.innerHTML = 'reply';
-    const likeButton = document.createElement('button');
-    likeButton.className = 'like';
-    likeButton.innerHTML = 'likes (0)';
-    const deleteButton = document.createElement('button');
-    deleteButton.className = 'delete';
-    deleteButton.innerHTML = 'delete';
-    const commentWrapper = document.createElement('div');
-    commentWrapper.className = 'comment';
-    textBox.textContent = commentText;
-    posterName.textContent = nameField;
-    document.getElementById("new-comment").value = '';
-    document.getElementById("name").value = '';
+    const contentWrapper = document.createElement('div');
+    contentWrapper.className = 'message-content';
+        const posterName = document.createElement('h3');
+        posterName.className = 'name';
+        const textBox = document.createElement('p');
+        const replyButton = document.createElement('button');
+        replyButton.className = 'reply';
+        replyButton.innerHTML = 'Reply';
+        const likeButton = document.createElement('button');
+        likeButton.className = 'like';
+        likeButton.innerHTML = 'Likes (0)';
+        const deleteButton = document.createElement('button');
+        deleteButton.className = 'delete';
+        deleteButton.id = 'delete';
+        deleteButton.innerHTML = 'Delete';
+        const contractButton = document.createElement('button');
+        contractButton.className = 'contract';
+        contractButton.id = 'contract';
+        contractButton.innerHTML = '-';
+        const commentWrapper = document.createElement('div');
+        commentWrapper.className = 'comment';
+        textBox.textContent = commentText;
+        posterName.textContent = nameField;
+        document.getElementById("new-comment").value = '';
+        document.getElementById("name").value = '';
+        
     if(commentTarget == document.getElementById("comments")){
         commentWrapper.id = 'root-comment';
     }
@@ -47,7 +55,8 @@ function addComment(ev){
         console.log("removing");
         document.getElementById("nocomments").remove();
     }
-    commentWrapper.append(posterName, textBox,replyButton,likeButton,deleteButton);
+    contentWrapper.append(posterName, textBox,replyButton,likeButton,deleteButton, contractButton);
+    commentWrapper.append(contentWrapper);
     commentTarget.appendChild(commentWrapper);
     setOnLocalStorage();
 }
@@ -59,7 +68,7 @@ function setOnLocalStorage () {
 }   
 
 function hasClass(elem, className){
-    console.log(elem.className.split(' '));
+    //console.log(elem.className.split(' '));
     return elem.className.split(' ').indexOf(className) > -1;
 }
 document.getElementById('comments').addEventListener('click', 
@@ -67,16 +76,20 @@ document.getElementById('comments').addEventListener('click',
         if(hasClass(e.target, 'reply')) {
             removeReplyBox();
             document.getElementById("addComment").innerHTML = '';
-            commentTarget = e.target.parentElement;
+            commentTarget = e.target.parentElement.parentElement;
             const reply = document.createElement('div');
             reply.id = 'new-reply';
-            reply.innerHTML = "<b>Reply</b>" + addCommentHTML + "<button id='cancelReply'>cancel</button>";
+            reply.innerHTML = "<b>Reply</b>" + addCommentHTML;
             e.target.parentElement.appendChild(reply);
+            const cancelButton = document.createElement('button');
+            cancelButton.innerHTML = 'Cancel';
+            cancelButton.id = 'cancelReply';
             document.getElementById('submit').addEventListener('click', function(ev) {addComment(ev)});
+            document.getElementsByClassName('add-comment')[0].append(cancelButton);
             document.getElementById('cancelReply').addEventListener('click', function(ev) {setOnLocalStorage()});
         }
         else if(hasClass(e.target, 'delete')){
-            e.target.parentElement.remove();
+            e.target.parentElement.parentElement.remove();
             console.log(document.getElementById('comments').innerHTML);
             if(document.getElementById('comments').innerHTML == ''){
                 console.log(document.getElementById('comments').innerHTML);
@@ -87,17 +100,41 @@ document.getElementById('comments').addEventListener('click',
         else if(hasClass(e.target, 'like')){
             var number = parseInt(e.target.innerHTML.match(/\d+/)[0]);
             number = number + 1;
-            e.target.innerHTML = "likes (" + number + ")";
+            e.target.innerHTML = "Likes (" + number + ")";
             setOnLocalStorage();
         }
-        
+        else if(hasClass(e.target, 'contract')){
+            removeReplyBox();
+            e.target.className = 'expand';
+            e.target.id = 'expand';
+            e.target.innerHTML = '+';
+            var replies = e.target.parentElement.parentElement.childNodes;
+            var others = [].slice.call(replies, 1)
+            console.log(others);
+            others.forEach(element => {
+                element.style = "display:none";
+            });
+        }
+        else if(hasClass(e.target, 'expand')){
+            removeReplyBox();
+            e.target.className = 'contract';
+            e.target.id = 'contract';
+            e.target.innerHTML = '-';
+            var replies = e.target.parentElement.parentElement.childNodes;
+            var others = [].slice.call(replies, 1)
+            console.log(others);
+            others.forEach(element => {
+                element.style = "display:block";
+            });
+
+        }
     })
 
 function removeReplyBox(){
     commentTarget = document.getElementById("comments");
     if(document.getElementById('new-reply')){
         document.getElementById('new-reply').remove();
-        document.getElementById('addComment').innerHTML = "<b>Add a comment...</b><br/>"+addCommentHTML;
-        document.getElementById('submit').addEventListener('click', function(ev) {addComment(ev)});
     }
+    document.getElementById('addComment').innerHTML = "<b>Add a comment...</b><br/>"+addCommentHTML;
+        document.getElementById('submit').addEventListener('click', function(ev) {addComment(ev)});
 }
